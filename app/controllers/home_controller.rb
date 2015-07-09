e776e4dcc3d5d762dce1c07623859b00dce40b52
NguyenTanDuc <nguyen.tan.duc@framgia.com>
2015-07-09 10:54:24 +0700

    vnexpress

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,12 @@ require 'open-uri'
 class HomeController < ApplicationController
   def index
     agent = Mechanize.new
-    page = agent.get("http://vnexpress.net/")
+    page = agent.get("http://vnexpress.net")
     @list_news = page.search("ul.list_news li")
-    @list_title = []
-    @list_description = []
-    @list_news.each do |anews|
-      @list_title << anews.search(".title_news a")
-      @list_description << anews.search(".news_lead")
-    end
-    binding.pry
+    @title = @list_news.search(".title_news a.txt_link").collect(&:text)
+    @description = @list_news.search(".news_lead").collect(&:text)
+    @links = @list_news.search("a.txt_link").map {|link| link['href']}
+    @images = @list_news.search(".thumb img").map {|src| src['src']}
+    # page2 = page.link_with(text: @title[0]).click
   end
 end
